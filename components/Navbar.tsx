20b5d5b2ac91729600f960af37b264966911bbe7
martindzida <martin.d14@email.cz>
2022-10-03 12:47:48 +0200

    Navbar improvement

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,45 @@
 import React from 'react'
 import Link from 'next/link'
-import CartItemsCountBadge from './CartItemsCountBadge'
-import { ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { ShoppingCartIcon, Bars3Icon } from '@heroicons/react/24/outline'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center text-emerald-400 shadow-md p-5 mx-auto'>
-      <div className='flex items-center'>
-        <div className='px-2'>
-            <Link href='/'>
-              <a>Crystalbase</a>
-            </Link>
-        </div>
-        <div className='px-2'>
-            <Link href='/crystals'>
-              <a>Crystals</a>
-            </Link>
-        </div>
+    <nav className='w-full shadow-md p-5 mx-auto'>
+      <div className='sm:invisible bg-white text-emerald-400'>
+        <button onClick={() => {console.log('navbar dropdown')}}>
+          <Bars3Icon className='w-6 h-6'/>
+        </button>
       </div>
-      <div className='flex items-center'>
-        <div className='px-2'>
-          <Link href='/signin'>
-            <button className='bg-white text-emerald-400 hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Signin</button>
-          </Link>
-        </div>
-        <div className='px-2'>
-          <Link href='/login'>
-            <button className='bg-white text-emerald-400 hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Login</button>
-          </Link>
-        </div>
-        <div className='px-2'>
-          <Link href='/cart'>
-            <a>
-              <ShoppingCartIcon className='w-6 h-6'/>
-            </a>
-          </Link>
-        </div>
-      </div>
-    </div>
+      <ul className='flex justify-between invisible sm:visible items-center text-emerald-400'>
+          <li className='px-2'>
+              <Link href='/'>
+                <a>Crystalbase</a>
+              </Link>
+          </li>
+          <li className='px-2'>
+              <Link href='/crystals'>
+                <a>Crystals</a>
+              </Link>
+          </li>
+          <li className='px-2'>
+            <Link href='/signin'>
+              <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Signin</button>
+            </Link>
+          </li>
+          <li className='px-2'>
+            <Link href='/login'>
+              <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Login</button>
+            </Link>
+          </li>
+          <li className='px-2'>
+            <Link href='/cart'>
+              <a>
+                <ShoppingCartIcon className='w-6 h-6'/>
+              </a>
+            </Link>
+          </li>
+      </ul>
+    </nav>
   )
 }
 
