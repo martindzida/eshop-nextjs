c16d265b13a8512d970ff12d85f0fc861a100889
martindzida <martin.d14@email.cz>
2022-10-11 18:36:51 +0200

    Better login and signup forms, changing design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,51 +1,55 @@
-import React from 'react'
-import Link from 'next/link'
-import { Disclosure, Menu } from '@headlessui/react'
-import { ShoppingCartIcon, Bars3Icon } from '@heroicons/react/24/outline'
+import React from 'react';
+import Link from 'next/link';
+import {ShoppingCartIcon, Bars3Icon} from '@heroicons/react/24/outline';
 
 const Navbar = () => {
   return (
     <nav className='w-full shadow-md p-5 mx-auto'>
-        <div className='flex justify-between items-center text-emerald-400 font-medium'>
-          <div className='flex items-center'>
-            <div className='px-2'>
-                <Link href='/'>
-                  <a>Crystalbase</a>
-                </Link>
-            </div>
-            <div className='px-2 invisible sm:visible'>
-                <Link href='/crystals'>
-                  <a>Crystals</a>
-                </Link>
-            </div>
+      <div className='flex justify-between items-center text-emerald-400 font-medium'>
+        <div className='flex items-center'>
+          <div className='px-2'>
+            <Link href='/'>
+              <a>Crystalbase</a>
+            </Link>
           </div>
-          <div className='flex items-center'>
-            <div className='px-2 hidden sm:block'>
-              <Link href='/signin'>
-                <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Signin</button>
-              </Link>
-            </div>
-            <div className='px-2 hidden sm:block'>
-              <Link href='/login'>
-                <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Login</button>
-              </Link>
-            </div>
-            <div className='px-2 hidden sm:block'>
-              <Link href='/cart'>
-                <a>
-                  <ShoppingCartIcon className='w-6 h-6'/>
-                </a>
-              </Link>
-            </div>
-            <div className='px-2 sm:hidden'>
-              <button onClick={() => {console.log('navbar dropdown')}}>
-                <Bars3Icon className='w-6 h-6'/>
-              </button>
-            </div>
+          <div className='px-2 invisible sm:visible'>
+            <Link href='/crystals'>
+              <a>Crystals</a>
+            </Link>
           </div>
         </div>
+        <div className='flex items-center'>
+          <div className='px-2 hidden sm:block'>
+            <Link href='/signup'>
+              <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Sign up</button>
+            </Link>
+          </div>
+          <div className='px-2 hidden sm:block'>
+            <Link href='/login'>
+              <button className='hover:bg-emerald-400 hover:text-white border border-emerald-400 px-2 py-1'>Log in</button>
+            </Link>
+          </div>
+          <div className='px-2 hidden sm:block'>
+            <Link href='/cart'>
+              <a>
+                <ShoppingCartIcon className='w-6 h-6' />
+              </a>
+            </Link>
+          </div>
+          <div className='px-2 sm:hidden'>
+            <button
+              onClick={() => {
+                console.log('navbar dropdown');
+              }}
+            >
+              <Bars3Icon className='w-6 h-6' />
+            </button>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
